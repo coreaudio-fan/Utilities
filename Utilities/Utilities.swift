5a9afc95620b6f13da35887e5c6b25ff0ef0d716
CoreAudio Fan <coreaudio-fan@users.noreply.github.com>
2026-07-17 17:23:35 -0700

    Initial Commit

---
+++ Utilities/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  Utilities
+//
+//  Created by Jeffrey Moore on 7/17/26.
+//
+
+import Foundation
+
---
+++ UtilitiesTests/UtilitiesTests.swift
@@ -0,0 +1,19 @@
+//
+//  UtilitiesTests.swift
+//  UtilitiesTests
+//
+//  Created by Jeffrey Moore on 7/17/26.
+//
+
+import Testing
+@testable import Utilities
+
+struct UtilitiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
