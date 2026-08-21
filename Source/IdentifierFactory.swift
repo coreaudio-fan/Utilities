d722f9eeabd3cf6b970785d82507f26695f765a7
CoreAudio Fan <coreaudio-fan@users.noreply.github.com>
2026-08-21 15:24:39 -0700

    Update the commentary
    (cherry picked from commit aeb722a371ab215b2e753295b84adb98c6dd3295)

--- Source/IdentifierFactory.swift
+++ Source/IdentifierFactory.swift
@@ -12,8 +12,8 @@ import Synchronization
 ///	This makes each instance essentially its own namespace for ID values regardless of type. Keeping this straight is
 ///	the responsibility of the user.
 ///
-///	Implementations for sizes less than 32 are provided for completeness. It is highly recommened to use a 32 bit or
-///	larger type for ID to avoid running out of values prematurely which will terminate the program.
+///	Implementations for sizes less than 32 bit sare provided for completeness. It is highly recommened to use a 32 bit
+///	or larger type for ID to avoid running out of values prematurely which will terminate the program.
 public final class IdentifierFactory<IDType: BinaryInteger & AtomicRepresentable & Sendable>: Sendable {
 
 	///	Type type of the identifiers
@@ -23,56 +23,69 @@ public final class IdentifierFactory<IDType: BinaryInteger & AtomicRepresentable
 	@inlinable public static var sentinel: ID { 0 }
 
 	///	Use an atomic counter that is only ever incremented to be thread safe cheaply. This also guarantees that
-	///	makeIdentifier() returns a value that is not the sentinel, has never been returned before and wont be
+	///	makeIdentifier() returns a value that is not the sentinel, has never been returned before and wont ever be
 	///	returned again.
 	private let previousID = Atomic<ID>(0)
 
+	///	The main initializer
 	public init() {}
 
+	///	The makeIdentifier implementation for Ints.
 	public func makeIdentifier() -> ID where ID == Int {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for Int8s.
 	public func makeIdentifier() -> ID where ID == Int8 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for Int16s.
 	public func makeIdentifier() -> ID where ID == Int16 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for Int32s.
 	public func makeIdentifier() -> ID where ID == Int32 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for Int64s.
 	public func makeIdentifier() -> ID where ID == Int64 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for Int128s.
 	public func makeIdentifier() -> ID where ID == Int128 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for UInts.
 	public func makeIdentifier() -> ID where ID == UInt {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for UInt8s.
 	public func makeIdentifier() -> ID where ID == UInt8 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for UInt16s.
 	public func makeIdentifier() -> ID where ID == UInt16 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for UInt32s.
 	public func makeIdentifier() -> ID where ID == UInt32 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for UInt64s.
 	public func makeIdentifier() -> ID where ID == UInt64 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
 
+	///	The makeIdentifier implementation for UInt128s.
 	public func makeIdentifier() -> ID where ID == UInt128 {
 		previousID.add(1, ordering: .relaxed).newValue
 	}
